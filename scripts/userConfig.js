ecd900c117f630a4984d7f1e47a2a093bc138dbc
Kiko Beats <josefrancisco.verdu@gmail.com>
2017-12-23 14:18:06 +0100

    Fix typo

--- scripts/userConfig.js
+++ scripts/userConfig.js
@@ -3,7 +3,7 @@
 const path = require('path')
 const processPkg = require(path.resolve(process.cwd(), 'package.json'))
 
-const lambapackConfig = processPkg.lambapack || {}
+const lambapackConfig = processPkg.lambdapack || {}
 const { whitelist = [], blacklist = [] } = lambapackConfig
 
 module.exports = { whitelist, blacklist }
